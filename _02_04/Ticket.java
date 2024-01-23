3fbb4e36e42bb6a69820668b76d2b9eb1456f7b4
toniadm <29669529+toniadm@users.noreply.github.com>
2024-01-23 16:27:37 +0000

    Method Creation

--- _02_04/Ticket.java
+++ _02_04/Ticket.java
@@ -10,10 +10,36 @@ public class Ticket {
   private double price;
   private boolean isReturn;
 
+  
   // Add three methods to set the value of each field, called setDestination,
   // setPrice and setIsReturn.
+  public void setDestination(String destination) {
+    this.destination = destination;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public void setIsReturn(boolean isReturn) {
+    this.isReturn = isReturn;
+  }
+
+  Ticket ticket = new Ticket();
 
   // Add a separate method to get the value of each field, called getDestination,
   // getPrice and getIsReturn.
+
+  public String getDestination() {
+    return destination;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public boolean getIsReturn() {
+    return isReturn;
+  }
 
 }
