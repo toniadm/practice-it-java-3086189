af44af4e081429b5965d39181a7d45752fb3ac54
toniadm <29669529+toniadm@users.noreply.github.com>
2024-01-23 18:53:09 +0000

    If else

--- _03_02/GradingSystem.java
+++ _03_02/GradingSystem.java
@@ -5,7 +5,12 @@ public class GradingSystem {
   public boolean isAPass(int percentage) {
     // Return true if the percentage is higher than or equal to 60.
     // Otherwise return false.
-    return false;
+    if (percentage >= 60) {
+      return true;
+    } else {
+      return false;
+    }
+    
   }
 
   public char getGrade(int percentage) {
@@ -14,8 +19,26 @@ public class GradingSystem {
     // If it's 70-79, return 'C'.
     // If it's 60-69, return 'D'.
     // If it's less than 60, return 'F'.
-    return 'X';
+
+    if (percentage >= 90) {
+      return 'A';
+
+    } else if (percentage >= 80) {
+      return 'B';
+
+    } else if (percentage >= 70) {
+      return 'C';
+      
+    } else if (percentage >= 60) {
+      return 'D';
+
+    } else {
+      return 'F';
+    }
+      
   }
+    
+
 
   public String retakeMessage(int percentage, boolean allowedToRetake) {
     // If percentage is less than 60 and allowedToRetake is true, return a String
@@ -24,7 +47,16 @@ public class GradingSystem {
     // that says "The student is not allowed to retake this exam."
     // If percentage is 60 or higher, return a String that says "A retake is not
     // required."
-    return "";
+    //return "";
+
+    if (percentage < 60 && allowedToRetake) {
+      return "The student has been entered for a retake";  
+    } else if (percentage < 60 && !allowedToRetake) {
+      return "The student is not allowed to retake this exam";  
+    } else {
+      return "A retake is not required.";
+      
+    }
   }
 
 }
